a31cf51a715afa3d80fbc6b6d90874124249aed3
xhemj <xhemj2680@163.com>
2022-07-08 17:43:25 +0800

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,8 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
+  target: "static",
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["ant-design-vue/dist/antd.css"],
 
