aa3ebd09d4fd6d35dbfaffded31def12ac361654
xhemj <xhemj2680@163.com>
2022-07-08 17:48:16 +0800

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    analyze: true,
+    // analyze: true,
     babelrc: true,
     extractCSS: true,
     optimization: {
