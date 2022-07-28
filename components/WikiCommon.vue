4abce0aef4fb1b4bfddaf1ba4bd942a353b235ad
xhemj <xhemj2680@163.com>
2022-07-28 13:04:35 +0800

    ServiceWorker 更改为 v3

--- components/WikiCommon.vue
+++ components/WikiCommon.vue
@@ -6,35 +6,35 @@
 export default {
   name: "WikiCommon",
   mounted() {
-    // function randomNumber(min, max) {
-    //   return Math.floor(Math.random() * (max - min + 1)) + min;
-    // }
-
-    // if ("serviceWorker" in navigator) {
-    //   window.addEventListener("load", function () {
-    //     navigator.serviceWorker
-    //       .register(
-    //         this.window.location.origin +
-    //           "/sw@v2.js?t=" +
-    //           randomNumber(100000, new Date().getTime())
-    //       )
-    //       .then(function () {
-    //         console.log("ServiceWorker registration successful");
-    //       })
-    //       .catch(function (err) {
-    //         console.log("ServiceWorker registration failed: ", err);
-    //       });
-    //   });
-    // }
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .getRegistrations()
-        .then(async function (registrations) {
-          for (const registration of registrations) {
-            await registration.unregister();
-          }
-        });
+    function randomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker
+          .register(
+            this.window.location.origin +
+              "/sw@v3.js?t=" +
+              randomNumber(100000, new Date().getTime())
+          )
+          .then(function () {
+            console.log("ServiceWorker registration successful");
+          })
+          .catch(function (err) {
+            console.log("ServiceWorker registration failed: ", err);
+          });
+      });
+    }
+    // if ("serviceWorker" in navigator) {
+    //   navigator.serviceWorker
+    //     .getRegistrations()
+    //     .then(async function (registrations) {
+    //       for (const registration of registrations) {
+    //         await registration.unregister();
+    //       }
+    //     });
+    // }
   },
 };
 </script>
