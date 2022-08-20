9b67779d72bc1acad72846d869bcbdd94d4163c2
xhemj <xhemj2680@163.com>
2022-08-20 18:44:39 +0800

    添加别名组件

---
+++ components/W/Img.vue
@@ -0,0 +1,24 @@
+<template>
+  <WikiPicture :src="src" :title="title" />
+</template>
+
+<script>
+import WikiPicture from "../WikiPicture.vue";
+
+export default {
+  name: "WImg",
+  components: {
+    WikiPicture,
+  },
+  props: {
+    src: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
