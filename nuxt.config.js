2efd129ec3ac4bc92f9381de363381753ae3eca1
xhemj <xhemj2680@163.com>
2022-08-22 17:24:38 +0800

    日期样式更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ const version = require("./package.json").version;
 const dayjs = require("dayjs");
 require("dayjs/locale/zh-cn");
 dayjs.locale("zh-cn");
-const time = dayjs().format("YYYY-M-D HH:mm:ss");
+const time = dayjs().format("YYYY-MM-DD HH:mm:ss");
 
 const createSitemapRoutes = async () => {
   let routes = [];
