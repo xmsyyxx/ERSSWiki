3f16137ec4f8199de9fa75145952ccc2c4690bc2
xhemj <xhemj2680@163.com>
2022-08-22 22:59:01 +0800

    puppeteer 预渲染画面

--- scripts/preFetch.js
+++ scripts/preFetch.js
@@ -1,41 +1,67 @@
 const cheerio = require("cheerio");
 const { $fetch } = require("ohmyfetch");
+const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 const log = console.log;
 
-const ENDPOINT = "http://127.0.0.1:2333";
+const ENDPOINT = "http://127.0.0.1:5500";
 const TARGET_PATH = path.resolve(__dirname, "../dist");
 const TARGET_FOLDER = ["item", "wiki"];
 
-/**
- * TODO:
- * 由于 Nuxt 自带的服务端渲染总是报错，
- * 所以自己写服务端渲染进行替换
- */
-for (let name of TARGET_FOLDER) {
-  const files = fs.readdirSync(path.resolve(TARGET_PATH, name));
-  for (let item of files) {
-    const targetPath = ["/item", "/wiki"];
-    for (let pathname of targetPath) {
-      const url = `${ENDPOINT}${pathname}/${item}/`;
-      $fetch(url).then((res) => {
-        const newHtml = cheerio.load(res);
-        const contentBody = newHtml("#__nuxt").html();
+(async () => {
+  /**
+   * TODO:
+   * 由于 Nuxt 自带的服务端渲染总是报错，
+   * 所以自己写服务端渲染进行替换
+   */
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+
+  for (let name of TARGET_FOLDER) {
+    const files = fs.readdirSync(path.resolve(TARGET_PATH, name));
+    for (let item of files) {
+      const targetPath = ["/item", "/wiki"];
+      for (let pathname of targetPath) {
+        const url = `${ENDPOINT}${pathname}/${item}`;
+
+        await page.goto(url);
+        await page.waitForSelector(".wiki-item");
+        await page.waitForSelector("body > link[rel='stylesheet']");
+        // await page.waitForTimeout(100);
+
+        const contentBody = await page.evaluate(() => {
+          return document.querySelector("#__nuxt").innerHTML;
+        });
+        let loadCssUrls = await page.evaluate(() => {
+          return Array.from(
+            document.querySelectorAll("body > link[rel='stylesheet']")
+          ).map((element) => element.href);
+        });
+        loadCssUrls = loadCssUrls.map((url) => {
+          return `<link href="${url}" rel="stylesheet" type="text/css">`;
+        });
+        // log({ loadCssUrls });
+
         const originHtmlRaw = fs.readFileSync(
-          path.resolve(TARGET_PATH, name, item, "index.html"),
+          path.resolve(TARGET_PATH, name, item),
           "utf8"
         );
-        const originHtml = cheerio.load(originHtmlRaw);
-        originHtml("#__nuxt").html(contentBody);
-        originHtml("#__nuxt").attr("data-server-rendered", "true");
+        const $ = cheerio.load(originHtmlRaw);
+        $("#__nuxt").html(contentBody);
+        for (let html of loadCssUrls) {
+          $(html).appendTo("head");
+        }
         fs.writeFileSync(
-          path.resolve(TARGET_PATH, name, item, "index.html"),
-          originHtml.html(),
+          path.resolve(TARGET_PATH, name, item),
+          $.html(),
           "utf8"
         );
         log(`${item} 替换成功`);
-      });
+      }
     }
   }
-}
+  await browser.close();
+})();
