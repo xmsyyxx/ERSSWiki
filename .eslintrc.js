d045209385b20585008b394a3cc9eaa90046f4d5
xhemj <xhemj2680@163.com>
2022-08-23 21:05:02 +0800

    一些小改动

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,18 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: "@babel/eslint-parser",
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  extends: ["@nuxtjs", "plugin:nuxt/recommended", "prettier"],
   plugins: [],
   // add your custom rules here
-  rules: {},
-}
+  rules: {
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["error.vue"],
+      },
+    ],
+  },
+};
