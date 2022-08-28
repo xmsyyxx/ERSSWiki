64dfb30ad15eae31f28113ffd866b06e858870f9
xhemj <xhemj2680@163.com>
2022-08-28 20:23:01 +0800

    新增 列表显示

--- components/WikiAvatarCard.vue
+++ components/WikiAvatarCard.vue
@@ -2,7 +2,7 @@
   <div class="wiki-avatar">
     <div class="wiki-card__content">
       <div class="wiki-card__avatar">
-        <img :src="img" />
+        <WikiPicture :src="img" :clickable="false" />
       </div>
       <div class="wiki-card__name"><slot></slot></div>
     </div>
@@ -10,8 +10,13 @@
 </template>
 
 <script>
+import WikiPicture from "./WikiPicture.vue";
+
 export default {
   name: "WikiAvatarCard",
+  components: {
+    WikiPicture,
+  },
   props: {
     img: {
       type: String,
@@ -27,7 +32,8 @@ export default {
 .wiki-avatar {
   display: flex;
   justify-content: flex-start;
-  margin: 20px auto;
+  margin-top: 0;
+  margin-bottom: 0.8rem;
   overflow: hidden;
 }
 
@@ -35,7 +41,8 @@ export default {
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  width: 350px;
+  width: fit-content;
+  min-width: 300px;
   height: 80px;
   text-decoration: none;
   color: $wiki-description-black;
@@ -59,7 +66,29 @@ export default {
   .wiki-avatar {
     position: relative;
     width: calc(100% - 2rem);
-    margin: 0 1rem;
+    margin: 0.5rem 1rem;
+  }
+}
+</style>
+
+<style lang="scss">
+.wiki-avatar__list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  .wiki-avatar {
+    margin-right: 1rem;
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  .wiki-avatar__list {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin: 0;
   }
 }
 </style>
