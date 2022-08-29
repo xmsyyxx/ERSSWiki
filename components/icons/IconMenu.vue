125decd534ed10e22e076308109eac29fb03b1ea
xhemj <xhemj2680@163.com>
2022-08-29 09:00:48 +0800

    图标文件格式化

--- components/icons/IconMenu.vue
+++ components/icons/IconMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <i aria-label="icon: menu" class="wiki-icon"
+  <i class="wiki-icon"
     ><svg
       viewBox="64 64 896 896"
       data-icon="menu"
@@ -8,7 +8,6 @@
       fill="currentColor"
       aria-hidden="true"
       focusable="false"
-      class=""
     >
       <path
         d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"
--- components/icons/IconUp.vue
+++ components/icons/IconUp.vue
@@ -1,5 +1,5 @@
 <template>
-  <i aria-label="icon: caret-up" class="wiki-icon"
+  <i class="wiki-icon"
     ><svg
       viewBox="0 0 1024 1024"
       data-icon="caret-up"
@@ -8,7 +8,6 @@
       fill="currentColor"
       aria-hidden="true"
       focusable="false"
-      class=""
     >
       <path
         d="M858.9 689L530.5 308.2c-9.4-10.9-27.5-10.9-37 0L165.1 689c-12.2 14.2-1.2 35 18.5 35h656.8c19.7 0 30.7-20.8 18.5-35z"
